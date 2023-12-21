bbf5ad59f0007f987ebf0d9be94078c4e4855eff
unknown <ekopurnama250498@gmail.com>
2023-12-21 17:42:55 +0700

    firsh commit

--- models/models.go
+++ models/models.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 type Users struct {
-	UserId 			uint  `gorm:"primaryKey" json:"id"`
+	Id 			uint  `gorm:"primaryKey" json:"id"`
 	Username       string `gorm:"type:varchar(80);unique" form:"username" binding:"required"`
 	Email          string `gorm:"type:varchar(80);unique" form:"email" binding:"required"`
 	Password       []byte `gorm:"not null" json:"-"`
@@ -13,15 +13,15 @@ type Users struct {
 }
 
 type Role struct {
-	IdRole uint   `gorm:"primaryKey" json:"id"`
+	Id uint   `gorm:"primaryKey" json:"id"`
 	Role   string `gorm:"type:varchar(80)"`
 }
 
 type AuthUserTokens struct {
-	TokenId 	uint   `gorm:"primaryKey" json:"id"`
+	Id 	uint   `gorm:"primaryKey" json:"id"`
 	AccessToken string `gorm:"unique" binding:"required"`
 	RefeshToken string `gorm:"unique" binding:"required"`
-	UserId uint  `gorm:"not null" json:"user_id"`
+	UserId uint  `gorm:"not null" json:"usersId"`
 	Users Users `gorm:"foreignKey:UserId"`
 }
 
